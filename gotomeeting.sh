d59c244164edb62ac08ca0307a93f33727ebb1e4
Julian Simpson <simpsonjulian@gmail.com>
2016-12-07 15:32:01 +1300

    Add Missed exports

--- gotomeeting.sh
+++ gotomeeting.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 . env.sh
-export CITRIX_USERNAME
-export CITRIX_PASSWORD
-export CITRIX_CONSUMER_KEY
+export CITRIX_USERNAME CITRIX_PASSWORD CITRIX_CONSUMER_KEY CITRIX_LICENSE_KEY \
+  CITRIX_GROUP_KEY
 ./bin/python gotomeeting.py $@
